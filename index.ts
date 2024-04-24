467c1e734e02a86480695cfac3924fe1df0b4186
Rumaan Iqbal <161339664+Rumaan123@users.noreply.github.com>
2024-04-24 09:11:08 -0700

    added a console

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import inquirer from "inquirer";
 // 3) compare user input with computer generated number and show result
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("welcome to number gussesing game");
 
 const answers = await inquirer.prompt([{
     name: "userGuessedNumber",
